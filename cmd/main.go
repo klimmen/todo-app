df2ea46ffa934de2cde4c1dd37fd19a95ff144a7
Vasyly Kleimenov <klim030187@gmail.com>
2021-08-09 12:25:01 +0300

    add config

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,19 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/klimmen/todo-app"
+	"github.com/klimmen/todo-app/pkg/handler"
+)
+
 func main() {
+	fmt.Println("Hello world!")
+	handlers := new(handler.Handler)
 
+	srv := new(todo.Server)
+	if err := srv.Run("8080", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }
-
